b47b2f55f5d49a0a9d046273cf3703f2300cace5
Ross Angle <rokitna@hotmail.com>
2014-12-05 22:48:07 -0800

    Simplified era-tacit.js, but it's unclear whether the old complexity would have helped for implementing recording.

--- src/era-tacit.js
+++ src/era-tacit.js
@@ -43,295 +43,49 @@ function historyStackPlus( entry, historyStack ) {
         rest: historyStack.rest };
 }
 
-function coerceTimes( stack ) {
-    return {
+bytecodeWords[ "l" ] = function ( stack ) {
+    return { type: "result", result: {
         path: stack.path,
-        historyStack:
-            historyStackPlus( "coerceTimes", stack.historyStack ),
+        historyStack: historyStackPlus( "l", stack.historyStack ),
         val: stack.val === null ? null :
-            stack.val.type === "times" ?
-                { type: "right", right: stack.val } :
-                { type: "left", left: stack.val }
-    };
-}
-
-function assertDisperseTimes( stack ) {
-    if ( !(stack.val === null || stack.val.type === "times") )
-        throw new Error();
-    return {
-        first: {
-            path: { first: "timesFirst", rest: stack.path },
-            historyStack: { first: null, rest: stack.historyStack },
-            val: stack.val === null ? null : stack.val.first
-        },
-        second: {
-            path: { first: "timesSecond", rest: stack.path },
-            historyStack: { first: null, rest: stack.historyStack },
-            val: stack.val === null ? null : stack.val.second
-        }
-    };
-}
-
-function assertCollectTimes( first, second ) {
-    if ( !(first.path.first === "timesFirst"
-        && second.path.first === "timesSecond"
-        && (first.val === null) === (second.val === null)
-        // TODO: See if we should really check these using object
-        // identity.
-        && first.path.rest === second.path.rest
-        && first.historyStack.rest === second.historyStack.rest) )
-        throw new Error();
-    return {
-        path: first.path.rest,
-        historyStack: historyStackPlus(
-            { type: "times",
-                first: first.historyStack.first,
-                second: second.historyStack.first },
-            first.historyStack.rest ),
-        val: first.val === null ? null :
-            { type: "times", first: first.val, second: second.val }
-    };
-}
-
-function productAssocLeft( stack ) {
-    if ( !(stack.val === null
-        || (stack.val.type === "times"
-            && stack.val.second.type === "times")) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack: historyStackPlus(
-            "productAssocLeft", stack.historyStack ),
-        val: stack.val === null ? null :
-            { type: "times",
-                first: { type: "times",
-                    first: stack.val.first,
-                    second: stack.val.second.first },
-                second: stack.val.second.second }
-    };
-}
-
-function productAssocRight( stack ) {
-    if ( !(stack.val === null
-        || (stack.val.type === "times"
-            && stack.val.first.type === "times")) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack: historyStackPlus(
-            "productAssocRight", stack.historyStack ),
-        val: stack.val === null ? null :
-            { type: "times",
-                first: stack.val.first.first,
-                second: { type: "times",
-                    first: stack.val.first.second,
-                    second: stack.val.second } }
-    };
-}
-
-function isPlusType( val ) {
-    return val.type === "left" || val.type === "right";
-}
-
-function coercePlus( stack ) {
-    return {
-        path: stack.path,
-        historyStack:
-            historyStackPlus( "coercePlus", stack.historyStack ),
-        val: stack.val === null ? null :
-            isPlusType( stack.val ) ?
-                { type: "right", right: stack.val } :
-                { type: "left", left: stack.val }
-    };
-}
-
-function assertDispersePlus( stack ) {
-    if ( !(stack.val === null || isPlusType( stack.val )) )
-        throw new Error();
-    return {
-        left: {
-            path: { first: "plusLeft", rest: stack.path },
-            historyStack: { first: null, rest: stack.historyStack },
-            val: stack.val === null ? null :
-                stack.val.type === "left" ? stack.val.left : null
-        },
-        right: {
-            path: { first: "plusRight", rest: stack.path },
-            historyStack: { first: null, rest: stack.historyStack },
-            val: stack.val === null ? null :
-                stack.val.type === "left" ? null : stack.val.right
-        }
-    };
-}
-
-function assertCollectPlus( left, right ) {
-    if ( !(left.path.first === "plusLeft"
-        && right.path.first === "plusRight"
-        && (left.val === null || right.val === null)
-        // TODO: See if we should really check these using object
-        // identity.
-        && left.path.rest === right.path.rest
-        && left.historyStack.rest === right.historyStack.rest) )
-        throw new Error();
-    return {
-        path: left.path.rest,
-        historyStack: historyStackPlus(
-            { type: "plus",
-                left: left.historyStack.first,
-                right: right.historyStack.first },
-            left.historyStack.rest ),
-        val: left.val !== null ?
-                { type: "left", left: left.val } :
-            right.val !== null ?
-                { type: "right", right: right.val } :
-                null
-    };
-}
-
-function sumAssocLeft( stack ) {
-    if ( !(stack.val === null
-        || (isPlusType( stack.val )
-            && (stack.val.type === "left"
-                || isPlusType( stack.val.right )))) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack: historyStackPlus(
-            "sumAssocLeft", stack.historyStack ),
-        val: stack.val === null ? null :
-            stack.val.type === "left" ?
-                { type: "left", left: stack.val } :
-            stack.val.right.type === "left" ?
-                { type: "left", left:
-                    { type: "right", right: stack.val.right.left } } :
-                stack.val.right
-    };
-}
-
-function sumAssocRight( stack ) {
-    if ( !(stack.val === null
-        || (isPlusType( stack.val )
-            && (stack.val.type === "right"
-                || isPlusType( stack.val.left )))) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack: historyStackPlus(
-            "sumAssocLeft", stack.historyStack ),
-        val: stack.val === null ? null :
-            stack.val.type === "left" ?
-                (stack.val.left.type === "left" ?
-                    stack.val.left :
-                    { type: "right", right:
-                        { type: "left", left:
-                            stack.val.left.right } }) :
-                { type: "right", right: stack.val }
-    };
-}
-
-function distribute( stack ) {
-    if ( !(stack.val === null
-        || (stack.val.type === "times"
-            && isPlusType( stack.val.second ))) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack:
-            historyStackPlus( "distribute", stack.historyStack ),
-        val: stack.val === null ? null :
-            stack.val.second.type === "left" ?
-                { type: "left", left:
-                    { type: "times",
-                        first: stack.val.first,
-                        second: stack.val.second.left } } :
+            stack.val.type === "times"
+                && stack.val.second.type === "times" ?
                 { type: "right", right:
                     { type: "times",
-                        first: stack.val.first,
-                        second: stack.val.second.right } }
-    };
-}
-
-function merge( stack ) {
-    if ( !(stack.val === null || isPlusType( stack.val )) )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack: historyStackPlus( "merge", stack.historyStack ),
-        val: stack.val === null ? null :
-            stack.val.type === "left" ?
-                stack.val.left :
-                stack.val.right
-    };
-}
-
-function commuteTimes( stack ) {
-    if ( !(stack.val === null || stack.val.type === "times") )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack:
-            historyStackPlus( "commuteTimes", stack.historyStack ),
-        val: stack.val === null ? null :
-            { type: "times",
-                first: stack.val.second, second: stack.val.first }
-    };
-}
-
-function assertRight( stack ) {
-    if ( !(stack.val === null || stack.val.type === "right") )
-        throw new Error();
-    return {
-        path: stack.path,
-        historyStack:
-            historyStackPlus( "assertRight", stack.historyStack ),
-        val: stack.val === null ? null : stack.val.right
-    };
-}
-
-bytecodeWords[ "l" ] = function ( stack ) {
-    var coercedOuter = assertDispersePlus( coerceTimes( stack ) );
-    var coercedOuterDisperse =
-        assertDisperseTimes( coercedOuter.right );
-    var sum = assertDispersePlus(
-        sumAssocLeft(
-            assertCollectPlus(
-                coercedOuter.left,
-                distribute(
-                    assertCollectTimes(
-                        coercedOuterDisperse.first,
-                        coerceTimes(
-                            coercedOuterDisperse.second ) ) ) ) ) );
-    return { type: "result", result:
-        assertCollectPlus(
-            merge( sum.left ), productAssocLeft( sum.right ) ) };
+                        first: { type: "times",
+                            first: stack.val.first,
+                            second: stack.val.second.first },
+                        second: stack.val.second.second } } :
+                { type: "left", left: stack.val }
+    } };
 };
 
 bytecodeWords[ "r" ] = function ( stack ) {
-    var coercedOuter = assertDispersePlus( coerceTimes( stack ) );
-    var coercedOuterDisperse =
-        assertDisperseTimes( coercedOuter.right );
-    var distributed = assertDispersePlus(
-        distribute(
-            commuteTimes(
-                assertCollectTimes(
-                    coerceTimes( coercedOuterDisperse.first ),
-                    coercedOuterDisperse.second ) ) ) );
-    var sum = assertDispersePlus(
-        sumAssocLeft(
-            assertCollectPlus(
-                coercedOuter.left,
-                assertCollectPlus(
-                    commuteTimes( distributed.left ),
-                    commuteTimes( distributed.right ) ) ) ) );
-    return { type: "result", result:
-        assertCollectPlus(
-            merge( sum.left ), productAssocRight( sum.right ) ) };
+    return { type: "result", result: {
+        path: stack.path,
+        historyStack: historyStackPlus( "r", stack.historyStack ),
+        val: stack.val === null ? null :
+            stack.val.type === "times"
+                && stack.val.first.type === "times" ?
+                { type: "right", right:
+                    { type: "times",
+                        first: stack.val.first.first,
+                        second: { type: "times",
+                            first: stack.val.first.second,
+                            second: stack.val.second } } } :
+                { type: "left", left: stack.val }
+    } };
 };
 
 bytecodeWords[ "assert" ] = function ( stack ) {
     if ( stack.val !== null && stack.val.type !== "right" )
         return { type: "error", stack: stack };
-    return { type: "result", result: assertRight( stack ) };
+    return { type: "result", result: {
+        path: stack.path,
+        historyStack:
+            historyStackPlus( "assert", stack.historyStack ),
+        val: stack.val === null ? null : stack.val.right
+    } };
 };
 
 // Expressions for testing
