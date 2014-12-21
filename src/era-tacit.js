8942c479cef4b3091df6f34460334e64ebec69ca
Ross Angle <rokitna@hotmail.com>
2014-12-21 03:42:12 -0800

    In era-tacit.js, started a new implementation based on abstract interpretation over the calculus of structures.

--- src/era-tacit.js
+++ src/era-tacit.js
@@ -207,3 +207,556 @@ testBytecode( "r assert".split( " " ),
             second: { type: "c" } } } )
 
 */
+
+
+
+// TODO: Test the following code.
+//
+// NOTE: The following code is basically a complete restart with a
+// more ambitious treatment of the calculus of structures (CoS) and
+// multiplicative additive linear logic (MALL). As the code runs, it
+// doesn't exactly compute, but it may construct other computations
+// (along linear logic's atomic flows), and those computations can use
+// the same language. Their code can then be used for a kind of
+// deduction that *does* compute. To control the flow of information
+// and make the typing information very precise, all data during
+// deduction (besides the type and the code itself) is given a
+// repeatable name and manipulated via Boolean algebra rather than
+// encapsulated in MALL additive connectives.
+
+
+
+// commands:
+//
+// id
+// then <command> <command>
+// reverse <command>
+// first <command>
+// intro <atomName>
+// switch
+// rotl
+// assocl
+// timesUnitIntro
+// parUnitIntro
+// absorbAndOr <value>
+// absorbOrAnd <value>
+// copy
+// distributeTwo
+// distributeZero <value>
+//
+//
+// values:
+//
+// timesUnit
+// times <value> <value>
+// parUnit
+// par <value> <value>
+// positiveResourceAtom <atomName>
+// negativeResourceAtom <atomName>
+// andUnit
+// and <value> <value>
+// orUnit
+// or <value> <value>
+// positiveConditionAtom <atomName>
+// negativeConditionAtom <atomName>
+//
+// NOTE: Conditions only support information-preserving deduction so
+// we can use them to mask values. What would be represented in
+// classical linear logic as (a plus b) will be represented instead as
+// ((k resourceWhenever a) times (-k resourceWhenever b)). For now,
+// some rules may do deep comparisons of the the condition k.
+//
+// TODO: Add these, along with their rules listed below.
+//
+// resourceWhenever <condition> <value>
+// resourceUnless <condition> <value>
+//
+// TODO: To fully support MALL, we need some way to quantify over the
+// truth values of a condition. That way we can represent MALL's
+// additive disjunction [A plus B] as (existsCondition k: (k.A k,B)).
+
+
+var setlikes = [ {
+    hasAtoms: true,
+    hasSwitch: true,
+    positive: { join: "times", unit: "timesUnit",
+        atom: "positiveResourceAtom", unitIntro: "timesUnitIntro" },
+    negative: { join: "par", unit: "parUnit",
+        atom: "negativeResourceAtom", unitIntro: "parUnitIntro" }
+}, {
+    hasAtoms: true,
+    hasSwitch: false,
+    positive: { join: "and", unit: "andUnit",
+        atom: "positiveConditionAtom", unitIntro: "andUnitIntro" },
+    negative: { join: "or", unit: "orUnit",
+        atom: "negativeConditionAtom", unitIntro: "orUnitIntro" }
+} ];
+
+function matchSetlike( checkSetlikePolarity ) {
+    for ( var i = 0, n = setlikes.length; i++; i < n ) {
+        var setlike = setlikes[ i ];
+        if ( checkSetlikePolarity( setlike.positive ) )
+            return { setlike: setlike,
+                forward: setlike.positive,
+                backward: setlike.negative };
+        if ( checkSetlikePolarity( setlike.negative ) )
+            return { setlike: setlike,
+                forward: setlike.negative,
+                backward: setlike.positive };
+    }
+    return false;
+}
+
+function matchJoin( type ) {
+    return matchSetlike( function ( x ) {
+        return x.join === type;
+    } );
+}
+
+function matchUnit( type ) {
+    return matchSetlike( function ( x ) {
+        return x.unit === type;
+    } );
+}
+
+function matchAtom( type ) {
+    return matchSetlike( function ( x ) {
+        return x.atom === type;
+    } );
+}
+
+function matchUnitIntro( type ) {
+    return matchSetlike( function ( x ) {
+        return x.unitIntro === type;
+    } );
+}
+
+function isSameConditionAtom( a, b ) {
+    return a.type === b.type && (false
+        || a.type === "andUnit"
+        || a.type === "orUnit"
+        || (a.type === "positiveConditionAtom" && a.name === b.name)
+        || (a.type === "negativeConditionAtom" && a.name === b.name)
+    );
+}
+
+function isSameConditionTerm( a, b ) {
+    return isSameConditionAtom( a, b ) || (a.type === b.type
+        && (a.type === "or" || a.type === "and")
+        && isSameConditionTerm( a.first, b.first )
+        && isSameConditionTerm( a.second, b.second )
+    );
+}
+
+function negateConditionTerm( x ) {
+    if ( x.type === "andUnit" ) {
+        return { type: "orUnit" };
+    } else if ( x.type === "orUnit" ) {
+        return { type: "andUnit" };
+    } else if ( x.type === "positiveConditionAtom" ) {
+        return { type: "negativeConditionAtom", name: x.name };
+    } else if ( x.type === "negativeConditionAtom" ) {
+        return { type: "positiveConditionAtom", name: x.name };
+    } else if ( x.type === "and" ) {
+        var first = negateConditionTerm( x.first );
+        if ( first === null )
+            return null;
+        var second = negateConditionTerm( x.second );
+        if ( second === null )
+            return null;
+        return { type: "or", first: first, second: second };
+    } else if ( x.type === "or" ) {
+        var first = negateConditionTerm( x.first );
+        if ( first === null )
+            return null;
+        var second = negateConditionTerm( x.second );
+        if ( second === null )
+            return null;
+        return { type: "and", first: first, second: second };
+    } else {
+        return null;
+    }
+}
+
+function isOppositeConditionAtom( a, b ) {
+    function oneWay( a, b ) {
+        return (false
+            || (a.type === "andUnit" && b.type === "orUnit")
+            || (a.type === "positiveConditionAtom"
+                && b.type === "negativeConditionAtom"
+                && a.name === b.name)
+        );
+    }
+    return oneWay( a, b ) || oneWay( b, a );
+}
+
+function isOppositeConditionTerm( a, b ) {
+    function oneWay( a, b ) {
+        return (a.type === "and" && b.type === "or"
+            && isSameConditionTerm( a.first, b.first )
+            && isSameConditionTerm( a.second, b.second ));
+    }
+    return isOppositeConditionAtom( a, b ) ||
+        oneWay( a, b ) || oneWay( b, a );
+}
+
+function runCommand( state, reversed, command ) {
+    var recordableCommand =
+        reversed ? { type: "reverse", command: command } : command;
+    
+    var m;
+    if ( command.type === "id" ) {
+        // A
+        // ---
+        // A
+        
+        return state;
+    } else if ( command.type === "then" ) {
+        // A
+        // --- works as long as A --- A' and A' --- A''
+        // A'
+        
+        var state2 = runCommand( state, reversed, command.first );
+        return runCommand( state2, reversed, command.second );
+    } else if ( command.type === "reverse" ) {
+        // -A'
+        // --- works as long as A --- A'
+        // -A
+        
+        return runCommand( state, !reversed, command.command );
+    } else if ( command.type === "first" ) {
+        // (A B C ...)
+        // --- works for any connective () with at least one
+        //     substructure as long as A --- A'
+        // (A' B C ...)
+        
+        if ( matchJoin( state.type ) ) {
+            return { type: state.type,
+                first: runCommand(
+                    state.first, reversed, command.command ),
+                second: state.second };
+        } else {
+            throw new Error();
+        }
+    } else if ( command.type === "intro" ) {
+        // ()
+        // --- works for any setlike connective family () [] a -a
+        //     which supports atoms
+        // [a -a]
+        
+        if ( !reversed ) {
+            var m2;
+            if ( !(m2 = matchUnit( state.type )
+                && m2.setlike.hasAtoms
+                && state.type === m2.setlike.positive.unit) )
+                throw new Error();
+//            var accum = { val: { type: "id" } };
+//            var first = { type: m2.setlike.positive.atom,
+//                name: command.name,
+//                getAccum: function () {
+//                    return accum;
+//                },
+//                setAccum: function ( newAccum ) {
+//                    accum = newAccum;
+//                },
+//                record: function ( c ) {
+//                    accum.val = { type: "then",
+//                        first: { type: "reverse", command: c },
+//                        second: accum.val };
+//                } };
+//            var second = { type: m2.setlike.negative.atom,
+//                name: command.name,
+//                dominate: function ( positiveAtom ) {
+//                    accum.val = { type: "then",
+//                        first: accum.val,
+//                        second: positiveAtom.getAccum().val };
+//                    positiveAtom.setAccum( accum );
+//                },
+//                record: function ( c ) {
+//                    accum.val =
+//                        { type: "then", first: accum.val, second: c };
+//                } };
+            var first = { type: m2.setlike.positive.atom,
+                name: command.name };
+            var second = { type: m2.setlike.negative.atom,
+                name: command.name };
+            return { type: m2.setlike.negative.join,
+                first: first, second: second };
+        } else {
+            var m2;
+            if ( !(true
+                && m2 = matchJoin( state.type )
+                && state.type === m2.setlike.positive.join
+                && state.first.type === m2.setlike.positive.atom
+                && state.first.name === command.name
+                && state.second.type === m2.setlike.negative.atom
+                && state.second.name === command.name
+            ) )
+                throw new Error();
+//            state.second.dominate( state.first );
+            return { type: m2.setlike.negative.unit };
+        }
+    } else if ( command.type === "switch" ) {
+        // ([A B] C)
+        // --- works for any setlike connective family () [] which
+        //     supports switch
+        // [(A C) B]
+        
+        var m2;
+        if ( !(m2 = matchJoin( state.type )
+            && m2.setlike.hasSwitch
+            && state.type === m2.setlike.positive.join
+            && state.first.type === m2.setlike.negative.join) )
+            throw new Error();
+        return { type: m2.setlike.negative.join,
+            first: { type: m2.setlike.positive.join,
+                first: state.first.first,
+                second: state.second },
+            second: state.first.second };
+    } else if ( command.type === "rotl" ) {
+        // (A B C ... Y Z)
+        // --- works for any connective () with a rotated equivalent
+        //     []
+        // [B C ... Y Z A]
+        
+        if ( matchUnit( state.type ) ) {
+            return state;
+        } else if ( matchJoin( state.type ) ) {
+            return { type: state.type,
+                first: state.second,
+                second: state.first };
+        } else if ( matchAtom( state.type ) ) {
+            return state;
+        } else {
+            throw new Error();
+        }
+    } else if ( command.type === "assocl" ) {
+        // (A (B C))
+        // --- works for any associative binary connective ()
+        // ((A B) C)
+        
+        if ( !reversed ) {
+            if ( matchJoin( state.type )
+                && state.second.type === state.type ) {
+                
+                return { type: state.type,
+                    first: { type: state.type,
+                        first: state.first,
+                        second: state.second.first },
+                    second: state.second.second };
+            } else {
+                throw new Error();
+            }
+        } else {
+            if ( matchJoin( state.type )
+                && state.first.type === state.type ) {
+                
+                return { type: state.type,
+                    first: state.first.first,
+                    first: { type: state.type,
+                        first: state.first.second,
+                        second: state.second } };
+            } else {
+                throw new Error();
+            }
+        }
+    } else if ( m = matchUnitIntro( command.type ) ) {
+        // A
+        // --- works for any binary connective () with a unit
+        // (A ())
+        
+        if ( !reversed ) {
+            return { type: m.forward.join,
+                first: state,
+                second: { type: m.forward.unit } };
+        } else {
+            if ( state.type !== m.backward.join
+                || state.second.type !== m.backward.unit )
+                throw new Error();
+            return state.first;
+        }
+    } else if ( command.type === "absorbAndOr" ) {
+        // (a [a b])
+        // ---
+        // a
+        
+        if ( !reversed ) {
+            if ( !(true
+                && state.type === "and"
+                && state.second.type === "or"
+                && isSameConditionTerm(
+                    state.first, state.second.first )
+                && isSameConditionTerm(
+                    state.second.second, command.value )
+            ) )
+                throw new Error();
+            return state.first;
+        } else {
+            if ( !isSameConditionTerm( state, state ) )
+                throw new Error();
+            return { type: "or",
+                first: state,
+                second: { type: "and",
+                    first: state,
+                    second: command.value } };
+        }
+    } else if ( command.type === "absorbOrAnd" ) {
+        // [a (a b)]
+        // ---
+        // a
+        
+        if ( !reversed ) {
+            if ( !(true
+                && state.type === "or"
+                && state.second.type === "and"
+                && isSameConditionTerm(
+                    state.first, state.second.first )
+                && isSameConditionTerm(
+                    state.second.second, command.value )
+            ) )
+                throw new Error();
+            return state.first;
+        } else {
+            if ( !isSameConditionTerm( state, state ) )
+                throw new Error();
+            return { type: "and",
+                first: state,
+                second: { type: "or",
+                    first: state,
+                    second: command.value } };
+        }
+    } else if ( command.type === "copy" ) {
+        // a
+        // ---
+        // (a a)
+        
+        if ( !reversed ) {
+            if ( !isSameConditionTerm( state, state ) )
+                throw new Error();
+            return { type: "and", first: state, second: state };
+        } else {
+            if ( !(state.type === "or"
+                && isSameConditionTerm( state.first, state.second )) )
+                throw new Error();
+            return state.first;
+        }
+    } else if ( command.type === "distributeTwo" ) {
+        // (a [B C])
+        // --- works for () over [] and [] over ()
+        // [(a B) (a C)]
+        
+        var m2;
+        if ( !(m2 = matchJoin( state.type )
+            && m2.setlike.positive.join === "and") )
+            throw new Error();
+        
+        if ( !reversed ) {
+            if ( !(true
+                && isSameConditionTerm( state.first, state.first )
+                && state.second.type === m2.backward.join
+            ) )
+                throw new Error();
+            return { type: m2.backward.join,
+                first: { type: m2.forward.join,
+                    first: state.first,
+                    second: state.second.first },
+                second: { type: m2.forward.join,
+                    first: state.first,
+                    second: state.second.second } };
+        } else {
+            if ( !(true
+                && state.first.type === m2.backward.join
+                && state.second.type === m2.backward.join
+                && isSameConditionTerm(
+                    state.first.first, state.second.first )
+            ) )
+                throw new Error();
+            return { type: m2.backward.join,
+                first: state.first.first,
+                second: { type: m2.forward.join,
+                    first: state.first.second,
+                    second: state.second.second } };
+        }
+    } else if ( command.type === "distributeZero" ) {
+        // (a [])
+        // --- works for () over [] and [] over ()
+        // []
+        
+        if ( !reversed ) {
+            var m2;
+            if ( !(true
+                && m2 = matchJoin( state.type )
+                && m2.setlike.positive.join === "and"
+                && isSameConditionTerm( state.first, command.value )
+                && state.second.type === m2.backward.unit
+            ) )
+                throw new Error();
+            return state.second;
+        } else {
+            var m2;
+            if ( !(true
+                && m2 = matchUnit( state.type )
+                && m2.setlike.positive.join === "and"
+            ) )
+                throw new Error();
+            return { type: m2.backward.join,
+                first: command.value,
+                second: state };
+        }
+        
+        // TODO: Add these rules. (The notation k.a means
+        // resourceWhenever with a condition of `k` and a resource of
+        // `a`. Likewise, k,a means resourceUnless. On the left side
+        // of a k.a or k,a, every () and [] means and and or
+        // respectively; otherwise they mean times and par. The
+        // notations [+] and (+) represent the additive units.)
+        //
+        // A
+        // <--->  additive unit
+        // ().A
+        //
+        // J.K.A  -- binding to the right: J.(K.(A))
+        // <--->  additive associativity
+        // (J K).A
+        //
+        // ()
+        // <--->
+        // [].a
+        //
+        // (k.a k,a)
+        // <--->
+        // a
+        //
+        // K.A
+        // <--->
+        // -K,A
+        //
+        // k.(A B)
+        // <--->
+        // (k.A k.B)
+        //
+        // k.()
+        // <--->
+        // ()
+        //
+        // [k.[A B] k,[C D]]
+        // --->
+        // [[k.A k,C] (k.B k,D)]
+        //
+        // (k.[A B] k,[C D])
+        // --->
+        // [(k.A k,C) (k.B k,D)]
+    } else {
+        throw new Error();
+    }
+}
+/*
+
+[() ()]
+([() ()] [[] ()])
+[([() ()] []) ()]
+[[(() ()) []] ()]
+[(() ()) ()]
+[() ()]
+
+*/
