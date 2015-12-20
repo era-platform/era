3f61d059ad6fb8aa6a10b6e1e6b3bbc1f9931440
Ross Angle <rokitna@hotmail.com>
2015-12-20 04:37:00 -0800

    Travis: First attempt to set up Travis for testing and for building gh-pages automatically.

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This is almost entirely taken from
+# <http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/>.
+
+set -o errexit -o nounset
+
+#cd subdirectory
+
+#git init
+git config user.name "$(git log -1 --pretty=%an HEAD)"
+git config user.email "$(git log -1 --pretty=%ae HEAD)"
+
+git remote add rocketnia-upstream "https://$ROCKETNIA_GH_TOKEN@github.com/rocketnia/era.git"
+git fetch rocketnia-upstream
+git reset rocketnia-upstream/gh-pages
+
+node build-era.js -pms
+
+touch .
+
+git add -A .
+git commit -m "$(git log -1 --pretty=%B HEAD)"
+git push -q rocketnia-upstream HEAD:gh-pages
