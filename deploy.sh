3aeb4384fd72d05b08ee67f62cb8f200bcebc221
Ross Angle <rokitna@hotmail.com>
2015-12-21 02:39:57 +0000

    Travis: Travis is using the wrong commit message. Tried getting the message before resetting to the gh-pages branch.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ set -o errexit -o nounset
 git config user.name "$(git log -1 --pretty=%an HEAD)"
 git config user.email "$(git log -1 --pretty=%ae HEAD)"
 
+message="$(git log -1 --pretty=%B HEAD)"
+
 git remote add rocketnia-upstream "https://$ROCKETNIA_GH_TOKEN@github.com/rocketnia/era.git"
 git fetch rocketnia-upstream
 git reset rocketnia-upstream/gh-pages || true
@@ -21,5 +23,5 @@ cp gh-pages.gitignore .gitignore
 touch .
 
 git add -A .
-git commit -m "$(git log -1 --pretty=%B HEAD)"
+git commit -m "$message"
 git push -q rocketnia-upstream HEAD:refs/heads/gh-pages
