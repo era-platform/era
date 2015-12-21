29096761f0eef67cf4c9442d6361fe29d92309e0
Ross Angle <rokitna@hotmail.com>
2015-12-21 03:12:46 +0000

    Travis: Polished deploy.sh. Specifically, removed the `touch .`, moved the environment variable checks into the script proper, and wrote extensive comments.

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-# This is almost entirely taken from
+# Build the `master` branch, and deploy it to the `qh-pages` branch.
+
+# This is adapted from
 # <http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/>.
 
+# If there's an error during any command, exit the script. If any
+# unset environment variable is used, exit the script.
 set -o errexit -o nounset
 
-#cd subdirectory
+# We only proceed this deployment was triggered by a commit to
+# `master`.
+test "$TRAVIS_PULL_REQUEST" == "false" || exit 0
+test "$TRAVIS_BRANCH" == "master" || exit 0
 
-#git init
+# We're going to make a commit to the gh-pages branch that looks a lot
+# like the current commit on `master`.
 git config user.name "$(git log -1 --pretty=%an HEAD)"
 git config user.email "$(git log -1 --pretty=%ae HEAD)"
-
 message="$(git log -1 --pretty=%B HEAD)"
 
-git remote add rocketnia-upstream "https://$ROCKETNIA_GH_TOKEN@github.com/rocketnia/era.git"
-git fetch rocketnia-upstream
+# Without changing the state of the working copy, we reset to the
+# `gh-pages` branch on GitHub. If the branch doesn't exist, we proceed
+# anyway (`|| true`); we'll end up creating the branch ourselves.
+git remote add rocketnia-upstream \
+  "https://$ROCKETNIA_GH_TOKEN@github.com/rocketnia/era.git"
+git fetch -q rocketnia-upstream
 git reset rocketnia-upstream/gh-pages || true
 
-node build-era.js -pms
+# We build the dependencies of the Era demos. We replace the
+# `.gitignore` file with a version where these dependencies are not
+# ignored.
+echo Building dependencies of the Era demos...
+node build-era.js --build-penknife --minify --build-staccato
 cp gh-pages.gitignore .gitignore
+echo Finished building dependencies of the Era demos.
 
-touch .
+# TODO: The original does this "so that `git` considers all of our
+# local copies fresh." See if we need to do this.
+#touch .
 
+# Commit all differences, including untracked files, to the `gh-pages`
+# branch. We specify the remote branch as `refs/heads/gh-pages` so
+# that this creates the branch if it doesn't exist yet.
 git add -A .
 git commit -m "$message"
 git push -q rocketnia-upstream HEAD:refs/heads/gh-pages
