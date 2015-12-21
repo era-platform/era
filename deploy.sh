e05f130aebd1b41d8527c1453b4d43de3cd64d16
Ross Angle <rokitna@hotmail.com>
2015-12-20 17:51:37 -0800

    Travis: Tried continuing with the deployment even if the remote gh-pages branch didn't exist.

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 # This is almost entirely taken from
 # <http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/>.
 
-echo blah3.1
-
 set -o errexit -o nounset
 
 #cd subdirectory
@@ -13,22 +11,19 @@ set -o errexit -o nounset
 git config user.name "$(git log -1 --pretty=%an HEAD)"
 git config user.email "$(git log -1 --pretty=%ae HEAD)"
 
-echo blah3.2
+echo blah1
 
 git remote add rocketnia-upstream "https://$ROCKETNIA_GH_TOKEN@github.com/rocketnia/era.git"
 git fetch rocketnia-upstream
-git reset rocketnia-upstream/gh-pages
+echo blah2
+git reset rocketnia-upstream/gh-pages || true
 
-echo blah3.3
+echo blah3
 
 node build-era.js -pms
-
-echo blah3.4
 
 touch .
 
 git add -A .
 git commit -m "$(git log -1 --pretty=%B HEAD)"
 git push -q rocketnia-upstream HEAD:gh-pages
-
-echo blah3.5
